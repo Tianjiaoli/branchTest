429278b1f5c4782c3e458923610bfb3014657d06
Tianjiao Li <Tianjiao.Li@red-gate.com>
2020-06-29 15:21:09 +0100

    Signed-off-by: Tianjiao Li <Tianjiao.Li@red-gate.com>

--- Tables/dbo.Persons.sql
+++ Tables/dbo.Persons.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[Persons]
 (
 [PersonID] [int] NOT NULL,
 [FirstName] [nvarchar] (50) COLLATE Latin1_General_CI_AS NULL,
-[LastName] [nvarchar] (50) COLLATE Latin1_General_CI_AS NULL
+[LastName] [nvarchar] (50) COLLATE Latin1_General_CI_AS NULL,
+[age] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Persons] ADD CONSTRAINT [PK__Persons__AA2FFB8538509973] PRIMARY KEY CLUSTERED  ([PersonID]) ON [PRIMARY]
